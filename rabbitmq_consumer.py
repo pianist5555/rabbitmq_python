17eb80d5fa61d535ccb9a38856c452dac5951325
seono park <pianist5555@gmail.com>
2023-04-30 00:47:40 +0900

    feat: 우선순위 큐 테스트

--- rabbitmq_consumer.py
+++ rabbitmq_consumer.py
@@ -12,8 +12,6 @@ def on_message(channel, method_frame, header_frame, body):
  
 connection = BlockingConnection()
 channel = connection.channel()
-#channel.basic_consume(queue='hello', on_message_callback=on_message)
-#channel.basic_consume(queue='world', on_message_callback=on_message)
 channel.basic_consume(queue='priority', on_message_callback=on_message)
  
 print("메시지 수신 대기 중")
--- rabbitmq_publisher.py
+++ rabbitmq_publisher.py
@@ -1,32 +1,27 @@
 from pika import BlockingConnection, BasicProperties, ConnectionParameters
- 
- 
-def message(topic, message):
-    connection = BlockingConnection()
-    try:
-        channel = connection.channel()
-        props = BasicProperties(content_type='text/plain', delivery_mode=1)
-        channel.basic_publish('incoming', topic, message, props) # incoming exchange로 publish
-    finally:
-        connection.close()
+import random
 
 
-def priority_message(topic, message):
+QUEUE_NAME = 'priority'
+
+def priority_message(topic, message, priority):
     connection = BlockingConnection()
     try:
         channel = connection.channel()
         props = BasicProperties(
-            priority=99,  # Set the message priority
-            content_type='text/plain', 
-            delivery_mode=1
+            priority=priority,           
         )
-        channel.basic_publish('incoming', topic, message, props)
+        channel.basic_publish(exchange='', routing_key=QUEUE_NAME, body=message, properties=props)
     except Exception as e:
         print(e)
     finally:
         connection.close()
- 
- 
-message('hello.1', 'hello task1')
-message('world.2', 'world task2')
-priority_message('priority.3', 'priority task3')
+
+
+for i in range(1,200):
+    priority = random.randrange(1,200)
+    priority_message(
+        f'priority.{priority}',
+        f'priority task{priority}',
+        priority
+    )
